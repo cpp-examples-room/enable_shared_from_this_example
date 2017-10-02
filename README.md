4232df3c102cd78cd669423eb8892e3ba2658208
x10mind <x10mind@yandex.ru>
2017-10-02 17:33:45 +0300

    update

--- README.md
+++ README.md
@@ -1,20 +1,8 @@
 # std::enable\_shared\_from\_this example
 
-## Tools installing
-You need install Xcode Command Line Tools (macOS) and
-run the next command:
-
-```bash
-git clone https://github.com/x10mind-cpp-examples/enable_shared_from_this_example.git
-cd enable_shared_from_this_example 
-./install_tools.sh
-```
-
 ## Building and running
-Run the next command from root project directory:
-
 ```bash
-polly/bin/build.py --verbose && _builds/default/enable_shared_from_this_example
+cmake -H. -B_builds && make -C _builds && _builds/enable_shared_from_this
 ```
 
 ## References
--- main.cpp
+++ main.cpp
@@ -3,25 +3,28 @@
 #include <iostream>
 
 class Foo final: public std::enable_shared_from_this<Foo> {
-    std::vector<std::shared_ptr<Foo>> objects_;
 public:
     Foo() noexcept {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        std::cout << __func__ << std::endl;
     };
 
     ~Foo() {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
-        std::cout << objects_.size() << std::endl;
+        std::cout << __func__ << std::endl;
+        std::cout << "size == " << objects_.size() << std::endl;
     }
 
     void process() {
+        std::cout << __func__ << std::endl;
         objects_.emplace_back(shared_from_this());
-        std::cout << objects_.size() << std::endl;
+        std::cout << "size == " << objects_.size() << std::endl;
     }
 
     void clear() {
         objects_.clear();
     }
+
+private:
+    std::vector<std::shared_ptr<Foo>> objects_;
 };
 
 int main() {
